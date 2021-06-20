762aad64e1350b0238d65cd38ce9bda14caa0050
Ravindra Nath <ravindranath@Ravindras-MacBook-Air.local>
2021-06-20 21:56:40 +0530

    fix: image size

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Key and api paths stored in the const for future use
 const KEY = "3fd2be6f0c70a2a598f084ddfb75487c";
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${KEY}&page=`;
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const IMG_PATH = "https://image.tmdb.org/t/p/w500";
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=`;
 
 // getting the access of the DOM elemnets by get element by id
@@ -54,9 +54,8 @@ const showMovies = (movies) => {
             <img
                 src="${IMG_PATH + poster_path}"
                 alt="${title}"
-                srcset="${IMG_PATH + poster_path} 360w, 
-                sizes="20vw"
-            />
+                srcset="${IMG_PATH + poster_path}"
+                            />
             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class="${getClassByRate(
--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 // Key and api paths stored in the const for future use
 const KEY = "3fd2be6f0c70a2a598f084ddfb75487c";
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${KEY}&page=`;
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const IMG_PATH = "https://image.tmdb.org/t/p/w300";
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=`;
 
 // getting the access of the DOM elemnets by get element by id
